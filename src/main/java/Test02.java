6a55dc4e48d90cdd83c53414a3c323d9dfcab3dc
Yevgen Kharchenko <yevgen.kharchenko.jd@gmail.com>
2019-10-03 15:23:09 +0300

    02 My Collection (my LinkedList)

---
+++ src/main/java/Test02.java
@@ -0,0 +1,14 @@
+public class Test02 {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(5);
+        myLinkedList.add(7);
+        myLinkedList.add(0);
+
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(3));
+    }
+}
